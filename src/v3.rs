5363b674c2211d17cf46d40a11ccd05f72201748
jan <jan.peltner@ggmgastro.com>
2025-08-14 02:50:37 +0200

    Implement various `std::ops` traits for `V3`

--- src/v3.rs
+++ src/v3.rs
@@ -17,3 +17,91 @@ impl V3 {
         self.len_sqr().sqrt()
     }
 }
+
+impl std::ops::Add for V3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        V3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Add for &V3 {
+    type Output = V3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        V3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Sub for V3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        V3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl std::ops::Sub for &V3 {
+    type Output = V3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        V3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for V3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        V3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl std::ops::Div<f64> for &V3 {
+    type Output = V3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+impl std::ops::Div<f64> for V3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+impl std::ops::Mul<f64> for &V3 {
+    type Output = V3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        V3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
