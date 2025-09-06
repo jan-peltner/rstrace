d7b288cad1c56c4198d8506030463f1c5d95ee4c
jan <defPelly@proton.me>
2025-09-06 13:03:34 +0200

    add fov90 test example

---
+++ examples/fov90.rs
@@ -0,0 +1,48 @@
+use rand::rngs::ThreadRng;
+use rstrace::camera::Camera;
+use rstrace::geometry::Sphere;
+use rstrace::ray::Hittables;
+use rstrace::vec::*;
+
+fn main() {
+    // --- Camera ---
+    let camera =
+        Camera::<ThreadRng>::with_default_rng(1600, 16.0 / 9.0, Point::zero(), 100, 10, 90.0);
+
+    let r = (std::f64::consts::PI / 4.0).cos();
+
+    let left_sphere = Box::from(Sphere::lambertian_with_albedo(
+        r,
+        Point {
+            x: -r,
+            y: 0.0,
+            z: -1.0,
+        },
+        Color {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0,
+        },
+    ));
+
+    let right_sphere = Box::from(Sphere::lambertian_with_albedo(
+        r,
+        Point {
+            x: r,
+            y: 0.0,
+            z: -1.0,
+        },
+        Color {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        },
+    ));
+
+    let world = Hittables {
+        objects: vec![left_sphere, right_sphere],
+    };
+
+    // --- Render ---
+    let _ = camera.render(world, "fov90sin.ppm");
+}
