bafa4b5737ce95e536df6dd1c31c8b3bd8432c95
jan <jan.peltner@ggmgastro.com>
2025-09-08 22:43:39 +0200

    add example for camera pose

---
+++ examples/metal_pose.rs
@@ -0,0 +1,92 @@
+use rand::rngs::ThreadRng;
+use rstrace::camera::{Camera, CameraPose};
+use rstrace::geometry::Sphere;
+use rstrace::ray::Hittables;
+use rstrace::vec::*;
+
+fn main() {
+    // --- Camera ---
+    let pose = CameraPose {
+        lookfrom: Vec3 {
+            x: 2.0,
+            y: 2.0,
+            z: 1.0,
+        },
+        lookat: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: -1.0,
+        },
+        vup: Vec3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        },
+    };
+    let camera = Camera::<ThreadRng>::with_default_rng(1600, 16.0 / 9.0, 100, 10, 90.0, pose);
+
+    // --- World ---
+    let world_sphere = Box::from(Sphere::lambertian_with_albedo(
+        100.0,
+        Point {
+            x: 0.0,
+            y: -100.5,
+            z: -1.0,
+        },
+        Color {
+            x: 0.8,
+            y: 0.8,
+            z: 0.0,
+        },
+    ));
+
+    let center_sphere = Box::from(Sphere::lambertian_with_albedo(
+        0.5,
+        Point {
+            x: 0.0,
+            y: 0.0,
+            z: -1.2,
+        },
+        Color {
+            x: 0.1,
+            y: 0.2,
+            z: 0.5,
+        },
+    ));
+
+    let left_sphere = Box::from(Sphere::metal_with_albedo(
+        0.5,
+        Point {
+            x: -1.0,
+            y: 0.0,
+            z: -1.0,
+        },
+        Color {
+            x: 0.8,
+            y: 0.8,
+            z: 0.8,
+        },
+        0.0,
+    ));
+    let right_sphere = Box::from(Sphere::metal_with_albedo(
+        0.5,
+        Point {
+            x: 1.0,
+            y: 0.0,
+            z: -1.0,
+        },
+        Color {
+            x: 0.8,
+            y: 0.6,
+            z: 0.2,
+        },
+        0.5,
+    ));
+
+    let world = Hittables {
+        objects: vec![center_sphere, world_sphere, left_sphere, right_sphere],
+    };
+
+    // --- Render ---
+    let _ = camera.render(world, "metal_spheres.ppm");
+}
