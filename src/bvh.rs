698e2bd0d8d0b97d96de6449eec6959d7d0eae3e
jan <jan.peltner@ggmgastro.com>
2025-10-01 15:20:57 +0200

    wip

--- src/bvh.rs
+++ src/bvh.rs
@@ -4,6 +4,11 @@ use crate::{
     utils::Interval,
 };
 
+// represents both individual nodes in the tree as well as the tree itself (root node)
+// tree structure:
+// root -> BvhNode
+// non-leaf-nodes -> BvhNode
+// leaf-nodes -> geometry primitives
 pub struct BvhNode {
     left: Box<dyn Hittable>,
     right: Box<dyn Hittable>,
@@ -12,10 +17,30 @@ pub struct BvhNode {
 
 impl Hittable for BvhNode {
     fn hit(&self, ray: &Ray3, t_range: &mut Interval) -> Option<Hit> {
-        todo!()
+        if !self.bbox().hit(ray, t_range) {
+            return None;
+        }
+
+        let hit_left = self.left.hit(ray, t_range);
+
+        if let Some(hit_left) = self.left.hit(ray, t_range) {
+            if hit_left.t < t_range.max {
+                t_range.max = hit_left.t
+            }
+        }
+
+        let hit_right = self.right.hit(ray, t_range);
+
+        hit_left.or(hit_right).or(None)
     }
 
     fn bbox(&self) -> AABB {
         self.bbox
+    }
+}
+
+impl BvhNode {
+    fn build_tree() -> Self {
+        todo!()
     }
 }
