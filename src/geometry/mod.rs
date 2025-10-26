0d1203047172aa87cd6383ea9a4f2f1b0d604644
jan <defPelly@proton.me>
2025-10-26 23:03:35 +0100

    scaffolding rotation

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,5 +1,7 @@
 mod quad;
+mod rot;
 mod sphere;
 
 pub use quad::*;
+pub use rot::*;
 pub use sphere::*;
---
+++ src/geometry/rot.rs
@@ -0,0 +1,34 @@
+use std::rc::Rc;
+
+use crate::{
+    aabb::AABB,
+    ray::{Hit, Hittable, Ray3},
+    utils::Interval,
+};
+
+#[derive(Debug)]
+pub struct RotateY<H: Hittable> {
+    object: H,
+    cos_theta: f64,
+    sin_theta: f64,
+}
+
+impl<H: Hittable> Hittable for RotateY<H> {
+    fn hit(&self, ray: &Ray3, t_range: &mut Interval) -> Option<Hit> {
+        todo!()
+    }
+
+    fn bbox(&self) -> AABB {
+        todo!()
+    }
+}
+
+impl<H: Hittable> RotateY<H> {
+    pub fn new(object: H, angle: f64) -> Self {
+        todo!()
+    }
+
+    pub fn new_rc(object: H, angle: f64) -> Rc<Self> {
+        Rc::from(Self::new(object, angle))
+    }
+}
